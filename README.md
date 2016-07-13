4d6a5cf65cefbedbd052b51dcbf71cd291b7134c
Scott C Reynolds <scott@scottcreynolds.com>
2016-07-13 15:05:48 -0400

    updates for issues (#10)
    * solution
    
    * update structure, tests
    
    * wip
    
    * completed conversion to es6 classes
    
    * Fix README and specs
    
    * merge, resolve conflicts, etc
    
    * update readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Presidents
 
 ## Objectives
+
 + Create Constructor Functions
 + Add methods to a constructor function
 + Create objects from constructor function
@@ -9,21 +10,18 @@
 
 We're tutoring a high school student in US History, and they have to memorize all the presidents of the United States, and relevant information. You want to make studying fun, so you plan to build a little app to help quiz them. For now, we're just going to build out the database.
 
-In `js/script.js`, build a president constructor function with following attributes: `name`, `politicalParty`, `yearsInOffice`, and `homeState`.
+In `index.js`, build an ES6 `President` class with following properties set by the constructor: `name`, `politicaParty`, `yearsInOffice`, and `homeState`.
 
-Add following methods to the prototype:
+Add following methods to the class:
 
 + `veto` - returns "NO!"
 
 + `passBill` - returns "You can do that!"
 
-+ `doCharity` - returns "I like to help people."
++ `doCharity` - returns "I like to help people"
 
-+ `conductPressInterview` - returns "I am proud to be an American."
++ `conductPressInterview` - returns "I am proud to be an american"
 
-+ `sayHi` - returns "Hi, my name is <name>. I am from <homeState>. I represent the <politicalParty>s, and was in office <yearsInOffice>.
++ `sayHi` - returns "Hi, my name is <name>. I am from <homestate>. I represent the <politcalParty>s and was in office <yearsInOffice>."
 
 Then create the following presidents: George Washington, Abraham Lincoln, Richard Nixon, and Jimmy Carter.
-
-
-<p data-visibility='hidden'>View <a href='https://learn.co/lessons/js-object-methods-lab'>Object Methods in JS Lab</a> on Learn.co and start learning to code for free.</p>
---
+++ test/index-test.js
@@ -0,0 +1,62 @@
+const expect = require('expect')
+
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+const babel = require('babel-core')
+
+const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
+  presets: ['es2015']
+})
+
+describe('index', () => {
+
+  jsdom({
+    src: babelResult.code
+  })
+
+  let lyndonJohnson
+  before(() => {
+    lyndonJohnson = new President("Lyndon B Johnson", "Democrat", "1963-1969", "Texas")
+  })
+
+  describe('Presidents Constructor Function', function() {
+    it('can create a president with a name, home state, political party, and years in office', function() {
+      expect(lyndonJohnson).toBeA(President)
+      expect(lyndonJohnson.name).toEqual("Lyndon B Johnson")
+      expect(lyndonJohnson.politicalParty).toEqual("Democrat")
+      expect(lyndonJohnson.yearsInOffice).toEqual("1963-1969")
+      expect(lyndonJohnson.homeState).toEqual("Texas")
+    })
+  })
+
+  describe('#veto', function() {
+    it('returns "NO!"', function() {
+      expect(lyndonJohnson.veto()).toEqual("NO!")
+    })
+  })
+
+  describe('#passBill', function() {
+    it('returns "You can do that!"', function() {
+      expect(lyndonJohnson.passBill()).toEqual("You can do that!")
+    })
+  })
+
+  describe('#doCharity', function() {
+    it('returns "I like the help people."', function() {
+      expect(lyndonJohnson.doCharity()).toEqual("I like the help people.")
+    })
+  })
+
+  describe('#onductPressInterview', function() {
+    it('returns "I am proud to be an American."', function() {
+      expect(lyndonJohnson.conductPressInterview()).toEqual("I am proud to be an American.")
+    })
+  })
+
+  describe('#sayHi', function() {
+    it('returns "Hi, my name is <name>, I am from <homestate>. I represent the <politcalParty>s and am from <homeState>."', function() {
+      expect(lyndonJohnson.sayHi()).toEqual("Hi, my name is Lyndon B Johnson. I am from Texas. I represent the Democrats, and was in office 1963-1969.")
+    })
+  })
+})
