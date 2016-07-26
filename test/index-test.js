fd07d9a3445a796335793430f59e77824e8a2f68
Charles Pletcher <cpletcher2@gmail.com>
2016-07-26 16:55:56 -0400

    Update tests, fix spelling issues
    Fixes #14

--- test/index-test.js
+++ test/index-test.js
@@ -13,31 +13,31 @@ describe('Presidents Constructor Function', function() {
   })
 })
 
-describe('#veto', function() {
+describe('veto()', function() {
   it('returns "NO!"', function() {
     expect(lyndonJohnson.veto()).toEqual("NO!")
   })
 })
 
-describe('#passBill', function() {
+describe('passBill()', function() {
   it('returns "You can do that!"', function() {
     expect(lyndonJohnson.passBill()).toEqual("You can do that!")
   })
 })
 
-describe('#doCharity', function() {
-  it('returns "I like the help people."', function() {
-    expect(lyndonJohnson.doCharity()).toEqual("I like the help people.")
+describe('doCharity()', function() {
+  it('returns "I like to help people."', function() {
+    expect(lyndonJohnson.doCharity()).toEqual("I like to help people.")
   })
 })
 
-describe('#onductPressInterview', function() {
+describe('conductPressInterview()', function() {
   it('returns "I am proud to be an American."', function() {
     expect(lyndonJohnson.conductPressInterview()).toEqual("I am proud to be an American.")
   })
 })
 
-describe('#sayHi', function() {
+describe('sayHi()', function() {
   it('returns "Hi, my name is <name>, I am from <homestate>. I represent the <politcalParty>s and was in office <yearsInOffice>."', function() {
     expect(lyndonJohnson.sayHi()).toEqual("Hi, my name is Lyndon B Johnson. I am from Texas. I represent the Democrats, and was in office 1963-1969.")
   })
