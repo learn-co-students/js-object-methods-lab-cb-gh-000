e9bab4b8773135f81d1957333e2f1e04005c0f3b
Qin LIN <linq.sophie@gmail.com>
2017-02-13 13:34:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code solution here
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+
+  veto() {
+    return "NO!"
+  }
+
+  passBill() {
+    return "You can do that!"
+  }
+
+  doCharity() {
+    return "I like to help people."
+  }
+
+  conductPressInterview() {
+    return "I am proud to be an American."
+  }
+
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}. I represent the ${this.politicalParty}s, and was in office ${this.yearsInOffice}.`
+  }
+}
+
+var George_Washington = new President(1,2,3,4)
+var Abraham_Lincoln = new President(1,2,3,4)
+var Richard_Nixon = new President(1,2,3,4)
+var Jimmy_Carter = new President(1,2,3,4)
